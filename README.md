823281dbe8ddb4c1be180d95473dbd308d6a7f2d
DzejPi <dzejjpii@gmail.com>
2023-01-07 00:18:09 +0100

    Base project added.

---
+++ README.md
@@ -0,0 +1,2 @@
+# The Heat
+My videogame for Ludum Dare 52. More information later!
---
+++ scripts/GlobalVar.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+onready var sfx_node = $SfxPlayer
+onready var music_node = $MusicPlayer
+
+var is_game_over = false
+var is_game_paused = false
+
+
+#func _ready():
+#	pass
+
+
+#func _process(delta):
+#	pass
